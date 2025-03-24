a6f107d76ae94b9c5398a238ec16ed12eaeef5ea
LZX3 <2608810718@qq.com>
2025-03-24 14:18:46 +0800

    20250224.1

---
+++ 近物实验2/弗兰克赫兹/线性拟合计算器1.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+# y = a x + b
+
+x = np.arange(1, 12)
+y = np.array([6.5 ,11.5, 17, 22, 27, 32, 37, 42.5, 47.5, 52.5, 57.5])
+
+N = np.size(x) # 数据点个数
+
+t = 2.31 # t因子
+
+x_bar = np.mean(x)
+y_bar = np.mean(y)
+
+a = np.sum( (x-x_bar)*(y-y_bar) ) / np.sum( (x-x_bar)**2 ) # 斜率a的估计值
+b = y_bar - a*x_bar # 截距b的估计值
+
+sigma = np.sqrt( 1/(N-2) * np.sum( (y-b-a*x)**2 ) )
+s_x = np.sqrt( 1/N * np.sum( (x-x_bar)**2 ) )
+
+UA_a = t * sigma / np.sqrt(N) / s_x # 斜率a的A类不确定度
+UA_b = t * sigma / np.sqrt(N) * np.sqrt(1 + x_bar**2 / s_x**2) # 截距b的A类不确定度
+
+print("斜率a的估计值:a=",a)
+print("截距b的估计值:b=",b)
+print("斜率a的A类不确定度:UA_a=",UA_a)
+print("截距b的A类不确定度:UA_b=",UA_b)
+
+zc_a = (y[N-1]-y[0])/(N-1) # 逐差法斜率估计值
+zc_b = y_bar - zc_a * x_bar # 逐差法截距估计值
+
+print("zc_a:",zc_a)
+print("zc_b:",zc_b)
+
+
